a3713147be806d37134a607cfa4e1ae926e5a909
Maya <wo@campus.technion.ac.il>
2026-02-22 00:03:02 +0200

    added closure queries and script

---
+++ slurm_job_closure_computation.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH --job-name=multivalued_heuristic_search-eps
+#SBATCH --output=logs/%x_%A_%a.out
+#SBATCH --error=logs/%x_%A_%a.err
+#SBATCH --mem=4G
+#SBATCH --nodelist=newton8,newton9,protagoras # Request run only on nodes newton3/newton4/galileo1
+#SBATCH --array=1 # TODO check this every time!
+#SBATCH --mail-user=wo@cs.technion.ac.il
+#SBATCH --mail-type=ALL           # Valid values are NONE, BEGIN, END, FAIL, REQUEUE, ALL
+#SBATCH --cpus-per-task=1         # number of cores (treats)
+
+# Go to the submission directory
+cd $SLURM_SUBMIT_DIR
+
+# Read the line corresponding to this array task
+LINE=$(sed -n "${SLURM_ARRAY_TASK_ID}p" closure_queries.txt)
+
+export LD_LIBRARY_PATH=$HOME/boost_install/lib:$LD_LIBRARY_PATH
+
+
+# Split line into variables
+read -r MAP ALG GOAL E1 E2 MVH <<< "$LINE"
+STARTS="$REST"
+
+# Create unique output folder
+OUTDIR=output
+
+# Run your executable
+./build/MultivaluedHeuristicSearch \
+  --map resources/$MAP \
+  --algorithm $ALG \
+  --goal $GOAL \
+  --e1 $E1 \
+  --e2 $E2 \
+  --mvh $MVH \
+  > "${OUTDIR}/output_${MAP}_${GOAL}_${ALG}_${E1}_${E2}.txt" 2>&1
